65f141bd5599643fbbf12151eb1c01168e29b54b
emanoel.lopes <emanoel.lopes@vagas.com.br>
2021-01-25 07:18:34 -0300

    adição de plugin do babel para optional chaining

--- test/app.spec.js
+++ test/app.spec.js
@@ -22,28 +22,36 @@ const inputs = {
   }
 }
 
-describe('Return de the cheapest Hotel', () => {
-  it('should return "Parque as flores"', () => {
-    return getTheCheapestHotel(inputs[1].type, inputs[1].days).then(data => {
-      expect(data?.name).to.equal('Parque Das Flores');
+describe('Return de the cheapest Hotel after the following input', () => {
+  describe('Regular: 16Mar2020(mon), 17Mar2020(tues), 18Mar2020(wed)', () => {
+    it('should return "Parque as flores"', () => {
+      return getTheCheapestHotel(inputs[1].type, inputs[1].days).then(data => {
+        expect(data?.name).to.equal('Parque Das Flores');
+      });
     });
   });
 
-  it('should return "Jardim Botânico"', () => {
-    return getTheCheapestHotel(inputs[2].type, inputs[2].days).then(data => {
-      expect(data?.name).to.equal('Jardim Botânico');
+  describe('Regular: 20Mar2020(fri), 21Mar2020(sat), 22Mar2020(sun)', () => {
+    it('should return "Jardim Botânico"', () => {
+      return getTheCheapestHotel(inputs[2].type, inputs[2].days).then(data => {
+        expect(data?.name).to.equal('Jardim Botânico');
+      });
     });
   });
 
-  it('should return "Mar Atlântico"', () => {
-    return getTheCheapestHotel(inputs[3].type, inputs[3].days).then(data => {
-      expect(data?.name).to.equal('Mar Atlântico');
+  describe('Fidelidade: 26Mar2020(thur), 27Mar2020(fri), 28Mar2020(sat)', () => {
+    it('should return "Mar Atlântico"', () => {
+      return getTheCheapestHotel(inputs[3].type, inputs[3].days).then(data => {
+        expect(data?.name).to.equal('Mar Atlântico');
+      });
     });
   });
 
-  it('fails request', () => {
-    return getTheCheapestHotel().then(data => data).catch(e => {
-      expect(e).toBe('Enter at least one day in format DD-MM-YYYY');
+  describe('if any date value was not entered', () => {
+    it('should return the message: Enter at least one day in format DD-MM-YYYY', () => {
+      return getTheCheapestHotel().then(data => data).catch(e => {
+        expect(e).toBe('Enter at least one day in format DD-MM-YYYY');
+      });
     });
   });
 });
