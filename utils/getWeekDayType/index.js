ef0fe902950c7af8b81f37d58b4bacb8d638c793
emanoel.lopes <emanoel.lopes@vagas.com.br>
2021-01-25 08:10:39 -0300

    [fix] date formats

--- utils/getWeekDayType/index.js
+++ utils/getWeekDayType/index.js
@@ -1,14 +1,14 @@
 /*
-  0 to 4 = Week Days
-  5 and 6 = Weekend days
+  1 to 5 = Week Days
+  0 and 6 = Weekend days
   @param date string
   @format DD/MM/YYYY - example '23/01/2021'
 */
 
 function getWeekDayType(date) {
   if (typeof date !== 'string') return 'Invalid date';
-  const d = new Date(date.split('/').reverse().join('-')).getDay();
-  const weekDayType = (d === 5 || d === 6) ? 'weekend' : 'week';
+  const d = new Date(date.split('/').reverse().join('/')).getDay();
+  const weekDayType = (d === 0 || d === 6) ? 'weekend' : 'week';
   return weekDayType;
 }
 
